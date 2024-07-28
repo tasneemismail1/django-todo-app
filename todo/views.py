29dd52c6ba24b21c160b7879acd1cf132f05a095
tasneemismail1 <tasneemismail5057@gmail.com>
2024-07-28 12:10:35 +0300

    new changes

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
 from .models import Task
 
+
+
+
+
+class CustomLoginView(LoginView):
+    template_name='todo/login.html'
+    fields = '_all_'
+    redirect_authenticated_user=True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 class TaskList(ListView):
     model = Task
     context_object_name='tasks'
