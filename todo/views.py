32441df5445c9e4f274f547d78cccb740b2700cb
tasneemismail1 <tasneemismail5057@gmail.com>
2024-07-28 13:56:05 +0300

    Revert "Merge branch 'todo-authentication'"
    This reverts commit e4ee00751c1d30410bf8181295151c40970992c6, reversing
    changes made to 275ccbf2db55e6128412f7ac4cb8dd9f711242a3.

--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+
 from .models import Task
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 
@@ -23,57 +22,28 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
-    
-    
-class RegisterPage(FormView):
-    template_name='todo/register.html'
-    form_class=UserCreationForm
-    redirect_authenticated_user=True
-    success_url=reverse_lazy('tasks')
-    
-    def form_valid(self, form):
-        user=form.save()
-        if user is not None:
-            login(self.request, user)
-            return super(RegisterPage, self).form_valid(form)
-    
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('tasks')
-        return super(RegisterPage, self).get(*args, **kwargs)
-    
-class TaskList(LoginRequiredMixin, ListView):
+class TaskList(ListView):
     model = Task
-    context_object_name = 'tasks'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
-
-        return context
-class TaskDetail(LoginRequiredMixin, DetailView):
+    context_object_name='tasks'
+    
+class TaskDetail(DetailView):
     model=Task
     context_object_name= 'task'
     template_name='todo/task.html'
     
-class TaskCreate(LoginRequiredMixin, CreateView):
+class TaskCreate(CreateView):
     model = Task
-    fields = ['title', 'description', 'complete']
-    success_url=reverse_lazy('tasks')
-    
-    def  form_valid(self, form):
-        form.instance.user= self.request.user
-        return super(TaskCreate, self).form_valid(form)
-    
-
-class TaskUpdate(LoginRequiredMixin, UpdateView):
-    model = Task
-    fields = ['title', 'description', 'complete']
+    fields = '__all__'
     success_url=reverse_lazy('tasks')
 
 
-class DeleteView(LoginRequiredMixin, DeleteView):
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url=reverse_lazy('tasks')
+
+
+class DeleteView(DeleteView):
     model = Task
     context_object_name= 'task'
     success_url=reverse_lazy('tasks')
